01ec4807f87d1aeff20c088f01e33e94342b9868
SuryaNuchu <55738939+SuryaNuchu@users.noreply.github.com>
2023-01-02 23:43:35 -0600

    PUSH#2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,33 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+/* MENU SHOW */ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
 
-/*=============== SERVICES MODAL ===============*/
+showMenu('nav-toggle','nav-menu')
 
+/*----- ANIMATE -----*/
+// OVERLAY
+gsap.to(".first", 1.5, {delay: .5, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".second", 1.5, {delay: .7, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".third", 1.5, {delay: .9, top: "-100%", ease: Expo.easeInOut});
 
-/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+// IMG
+gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 60})
 
+// INFORMATION
+gsap.from('.home__information', {opacity: 0, duration: 3, delay: 2.3, y: 25})
+gsap.from('.anime-text', {opacity: 0, duration: 3, delay: 2.3, y: 25, ease:'expo.out', stagger: .3})
 
-/* Link active work */ 
+// NAV ITEM
+gsap.from('.nav__logo', {opacity:0, duration: 3, delay: 3.2, y: 25, ease:'expo.out'});
+gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease:'expo.out', stagger: .2})
 
-
-/*=============== SWIPER TESTIMONIAL ===============*/
-
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-
-/*=============== LIGHT DARK THEME ===============*/ 
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-
+// SOCIAL
+gsap.from('.home__social-icon', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})
